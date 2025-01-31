3d3b5a1eda18c6f3754dd5783d0c0b5ec5871636
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-01-31 18:40:10 +0500

    fix: remove darwin options

--- shell.nix
+++ shell.nix
@@ -3,29 +3,6 @@
 }:
 let
   getLibFolder = pkg: "${pkg}/lib";
-  getFramwork = pkg: "${pkg}/Library/Frameworks";
-  darwinOptions =
-    if pkgs.stdenv.isDarwin then
-      ''
-        -F${(getFramwork pkgs.darwin.apple_sdk.frameworks.Security)}
-        -F${(getFramwork pkgs.darwin.apple_sdk.frameworks.CoreFoundation)}
-        -F${(getFramwork pkgs.darwin.apple_sdk.frameworks.CoreServices)}
-        -F${(getFramwork pkgs.darwin.apple_sdk.frameworks.SystemConfiguration)}
-      ''
-    else
-      "";
-
-  darwinPkgs =
-    if pkgs.stdenv.isDarwin then
-      with pkgs;
-      [
-        darwin.apple_sdk.frameworks.Security
-        darwin.apple_sdk.frameworks.CoreServices
-        darwin.apple_sdk.frameworks.CoreFoundation
-        darwin.apple_sdk.frameworks.SystemConfiguration
-      ]
-    else
-      [ ];
 in
 pkgs.stdenv.mkDerivation {
   name = "registrar";
@@ -69,7 +46,7 @@ pkgs.stdenv.mkDerivation {
 
   # Set Environment Variables
   RUST_BACKTRACE = 1;
-  NIX_LDFLAGS = "-L${(getLibFolder pkgs.libiconv)} ${darwinOptions}";
+  NIX_LDFLAGS = "-L${(getLibFolder pkgs.libiconv)}";
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
     (getLibFolder pkgs.gcc)
