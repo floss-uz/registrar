5422ccec331cdda77a29bc20bb6a25136fad33d4
lambdajon <lambdajon42@gmail.com>
2025-10-11 07:58:38 +0500

    fix: short name as d

--- src/Registrar.hs
+++ src/Registrar.hs
@@ -13,7 +13,7 @@ import Options.Generic
 type Options :: Type -> Type
 data Options w = Options
   { port :: !(w ::: Int <?> "Port to listen on" <!> "9060" <#> "p")
-  , database :: !(w ::: B.ByteString <?> "Database connection string" <#> "u")
+  , database :: !(w ::: B.ByteString <?> "Database connection string" <#> "d")
   , databasePoolSize :: !(w ::: Int <?> "Database pool size" <!> "10" <#> "s")
   , migrations :: !(w ::: Bool <?> "Run migrations" <!> "False" <#> "m")
   }
