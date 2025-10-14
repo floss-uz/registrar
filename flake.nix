c8875019b6aa781bdd5ee311b639a1fc80729136
lambdajon <lambdajon42@gmail.com>
2025-10-14 16:12:35 +0500

    chore: remove strict containers and update flake

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
             esqueleto = hlib.dontCheck (hlib.doJailbreak super.esqueleto);
             optparse-generic = hlib.dontCheck (hlib.doJailbreak super.optparse-generic);
             postgresql-simple = hlib.dontCheck (hlib.doJailbreak super.postgresql-simple);
-            strict-container = hlib.dontCheck (hlib.doJailbreak super.strict-container);
+            strict-containers = hlib.dontCheck (hlib.doJailbreak super.strict-containers);
           };
         };
 
