6c16faba50cd2a4a8acfd9087c50a00719c2ff66
Kamron Khamrakulov <68609265+xfeusw@users.noreply.github.com>
2025-10-15 21:16:31 +0500

    Update src/Registrar/Prelude.hs
    Co-authored-by: λjon  <46736978+lambdajon@users.noreply.github.com>

--- src/Registrar/Prelude.hs
+++ src/Registrar/Prelude.hs
@@ -8,6 +8,6 @@ module Registrar.Prelude
 
 import Data.Text (Text)
 import GHC.Generics (Generic)
-import Data.Aeson (FromJSON, ToJSON, encode, decode)
+import Data.Aeson (FromJSON, ToJSON)
 import Control.Monad.IO.Class (MonadIO (..))
 import Data.Kind (Type, Constraint)
