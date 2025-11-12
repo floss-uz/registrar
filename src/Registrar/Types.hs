d2dd0190fdd0845ae73575d0fbf2f1959aa50d43
lambdajon <lambdajon42@gmail.com>
2025-11-12 15:24:19 +0500

    chore: implement WrapId type

--- src/Registrar/Types.hs
+++ src/Registrar/Types.hs
@@ -9,17 +9,24 @@ module Registrar.Types
   , CommunityId (..)
   , PoolSql (..)
   , TelegramAuth (..)
+  , WrapId (..)
   ) where
 
 import Registrar.Prelude
 
-import Data.Data (Proxy (..))
-import Data.OpenApi (ToSchema)
+import Data.Aeson (ToJSON (toJSON))
+import Data.Aeson.Types (Value (Object), (.=))
+import Data.Data (Proxy (..), Typeable)
+import Data.OpenApi (HasExample (example), HasSchema (schema), NamedSchema (..), Referenced (..), Schema, SchemaOptions (fieldLabelModifier), ToSchema (declareNamedSchema), declareSchema, defaultSchemaOptions, genericDeclareNamedSchema)
+import Data.OpenApi.Internal.ParamSchema (ToParamSchema)
 import Data.Pool (Pool)
+import Data.Typeable (Typeable)
 import Database.Esqueleto.Experimental
 import Database.Persist.SqlBackend (SqlBackend)
 import Database.Persist.TH
 import GHC.Records (HasField (..))
+import Registrar.Orphans
+import Servant.OpenApi (HasOpenApi (toOpenApi))
 
 type PoolSql :: Constraint
 type PoolSql = (?pool :: Pool SqlBackend)
@@ -35,12 +42,17 @@ share
     manager Text Maybe
     github Text
     website Text Maybe
-    deriving Eq
+    deriving Eq 
 |]
 
-type Community :: Type
 type CommunityId :: Type
 
+deriving stock instance Generic CommunityId
+deriving stock instance Typeable CommunityId
+deriving anyclass instance ToSchema CommunityId
+deriving anyclass instance ToParamSchema CommunityId
+
+type Community :: Type
 deriving stock instance Show Community
 deriving stock instance Generic Community
 deriving anyclass instance ToJSON Community
@@ -63,3 +75,28 @@ deriving stock instance Generic TelegramAuth
 deriving anyclass instance ToJSON TelegramAuth
 deriving anyclass instance FromJSON TelegramAuth
 deriving anyclass instance ToSchema TelegramAuth
+
+-- | This record for transforming entity to flat object.
+data WrapId record = WrapId
+  { recd :: record
+  , wKey :: Key record
+  }
+  deriving stock (Generic)
+
+deriving anyclass instance Typeable WrapId
+instance (ToJSON (Key record), ToJSON record) => ToJSON (WrapId record) where
+  toJSON WrapId{..} =
+    Object $
+      "id" .= wKey
+        <> case toJSON recd of
+          Object o -> o
+          _ -> mempty
+
+instance (ToSchema (Key record), ToSchema record) => ToSchema (WrapId record) where
+  declareNamedSchema _ = do
+    -- FIXME: implement key schema as {id: 1}. And change namedSchema to schema.
+    recSchema <- declareSchema (Proxy @record)
+    keySchema <- declareSchema (Proxy @(Key record))
+    let
+      fullSchm = recSchema <> keySchema
+    pure $ NamedSchema (Just "WrapId") $ fullSchm
