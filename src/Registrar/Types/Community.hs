e2a99e2cfbecf3fc7ae08017f6a7cbaa87f9e895
lambdajon <lambdajon42@gmail.com>
2025-11-19 23:56:15 +0500

    feat: Implement community type fro api handlers and bot

---
+++ src/Registrar/Types/Community.hs
@@ -0,0 +1,20 @@
+module Registrar.Types.Community where
+
+import Data.Int (Int64)
+import Data.OpenApi (ToSchema)
+import Registrar.Prelude
+
+data Community = MkCommunity
+  { id :: !Int64
+  , name :: !Text
+  , established :: !Text
+  , mission :: !Text
+  , chat :: !(Maybe Text)
+  , manager :: !(Maybe Text)
+  , github :: !Text
+  , website :: Maybe Text
+  }
+  deriving stock (Eq, Show, Generic)
+
+deriving anyclass instance ToJSON Community
+deriving anyclass instance ToSchema Community
